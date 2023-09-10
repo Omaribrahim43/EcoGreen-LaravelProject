bc58c892953c59ddcfbcbb02bdf0b795a8ad72a5
Omaribrahim43 <omar.migdadi43@gmail.com>
2023-09-10 22:10:05 +0300

    omar

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::get('authorized/google/callback', [LoginWithGoogleController::class, 'han
 
 
 //*******************Rania********************* */
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [HomeController::class, 'about'])->name('about');
 Route::get('/aboutus', [AboutUsController::class, 'home'])->name('home');
 //*******************Rania********************* */
