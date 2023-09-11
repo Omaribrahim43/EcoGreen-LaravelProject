ee14f956ddc4217453e9fe10abd3f6d2351afdf0
SaraAbabneh <sara.ababneh1997@gmail.com>
2023-09-11 12:26:16 -0700

    sara

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -15,19 +15,19 @@ class ProjectSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('projects')->insert([
-            [
-                'title' => 'Streets Of Irbid',
-                'image' => '',
-                'location' => 'City of Irbid',
-                'short_description' => 'Join us in our mission to make Irbid streets greener and more sustainable.',
-                'long_description' => 'Join us in our mission to make Irbid streets greener and more sustainable through our collvolunteers, and organizations to come together to make a lasting impact on our environment.',
-                'tree_type' => 'Dwarf Cedar',
-                'fertilizer' => 'No Need',
-                'equipments' => 'No Need',
-                'user_id' => '1',
-                'category_id' => '1',
-            ],
-        ]);
+        // DB::table('projects')->insert([
+        //     [
+        //         'title' => 'Streets Of Irbid',
+        //         'image' => '',
+        //         'location' => 'City of Irbid',
+        //         'short_description' => 'Join us in our mission to make Irbid streets greener and more sustainable.',
+        //         'long_description' => 'Join us in our mission to make Irbid streets greener and more sustainable through our collvolunteers, and organizations to come together to make a lasting impact on our environment.',
+        //         'tree_type' => 'Dwarf Cedar',
+        //         'fertilizer' => 'No Need',
+        //         'equipments' => 'No Need',
+        //         'user_id' => '1',
+        //         'category_id' => '1',
+        //     ],
+        // ]);
     }
 }
